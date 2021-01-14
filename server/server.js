b5ecf58f42ec5189932b7ec5261c9bd778383a0c
Gal Shaked <galsh1994@gmail.com>
2021-01-14 16:50:19 +0200

    add a script to run angular,react and node toghter

--- server/server.js
+++ server/server.js
@@ -5,15 +5,12 @@ const mongoose= require('mongoose');
 const Schema=mongoose.Schema;
 const loginRouter=require('./routes/loginRouter')
 
-
-
 const app = express();
 
 
 app.use(cors());
 app.use(bodyParser.json())
 app.use('/login',loginRouter)
-
 
 
 console.log("Server is runnig...");
