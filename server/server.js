383844d1f2de65e0cbb4f35788485e493617bf21
Gal Shaked <galsh1994@gmail.com>
2021-01-14 17:42:07 +0200

    change the readme and the server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,8 @@ const bodyParser = require ('body-parser');
 const cors= require('cors');
 const mongoose= require('mongoose');
 const Schema=mongoose.Schema;
-<<<<<<< HEAD
-const loginRouter=require('./routes/loginRouter')
-=======
 const userRouter=require('./routes/userRouter')
 
->>>>>>> b7ce8fc803fbb9cbbf0d2f8747f40927945afcb5
 
 const app = express();
 
