e422df0c9ff6bce71a6ee2ccdd0622a9318550c0
Gal Shaked <galsh1994@gmail.com>
2021-04-16 18:23:02 +0300

    add passport auth to react side

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -12,23 +12,23 @@ export default function LoginForm() {
 
   if (!loggedIn) {
     return (
-      <div  style={{fontFamily: "Open Sans"}}>
-       <section id="hero" className="d-flex align-items-center">
+      <div style={{ fontFamily: "Open Sans" }}>
+        <section id="hero" className="d-flex align-items-center">
           <div className="container position-relative text-center text-lg-left" data-aos="zoom-in" data-aos-delay={100}>
             <div className="wrapper fadeInDown">
               <div id="formContent">
                 <div className="fadeIn first">
-                    <img src={logo} id="icon" alt="Welcome Buddy" />
-                    <h1 style={{ fontSize: '35px', color: '#51361A' }}>Nice to see you again! </h1>
-                   {messege.showMessege ? <div>{messege.text}</div> : null}
+                  <img src={logo} id="icon" alt="Welcome Buddy" />
+                  <h1 style={{ fontSize: '35px', color: '#51361A' }}>Nice to see you again! </h1>
+                  {messege.showMessege ? <div>{messege.text}</div> : null}
                 </div>
                 <form onSubmit={(e) => { login(e, setLoggedIn, setMessege) }}>
                   <input type="text" id="email" className="fadeIn second" name="login" placeholder="Email" />
-                  <input  type="password" id="password" className="fadeIn third" name="login" placeholder="password" /> <br></br>
-                  <button style={{width:'120px',background: '#84996f'}}className="button" type="submit"><span>Log in</span></button><br></br>
+                  <input type="password" id="password" className="fadeIn third" name="login" placeholder="password" /> <br></br>
+                  <button style={{ width: '120px', background: '#84996f' }} className="button" type="submit"><span>Log in</span></button><br></br>
                 </form>
                 <div id="formFooter">
-                   <Link className="underlineHover" style={{ color: '#51361A' }} to="/register">Don't have an account? Register!</Link>
+                  <Link className="underlineHover" style={{ color: '#51361A' }} to="/register">Don't have an account? Register!</Link>
                 </div>
               </div>
             </div>
@@ -44,20 +44,37 @@ function login(e, setLoggedIn, setMessege) {
   e.preventDefault();
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
-  axios.get('http://localhost:8080/user/byemail/' + email).
-    then(Response => {
-      if (Response.data)
-        if (Response.data.password === password) {
-          window.sessionStorage.setItem('userID', Response.data._id);
-          setLoggedIn(true);
-        }
-        else
-          setMessege({ text: 'password incorrect', showMessege: true })
-      else
-        setMessege({ text: 'invalid email', showMessege: true })
+  var logIn = false;
 
+  fetch('http://localhost:8080/user/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email,
+      password
     })
+  }).then(data => { data.json(); logIn = true; console.log(logIn)},
+    error => console.error('wrong password or email')
+  ).then(()=>{
+    if (logIn===true) {
+      console.log('in')
+      axios.get('http://localhost:8080/user/byemail/' + email).
+        then(Response => {
+          if (Response.data) {
+            window.sessionStorage.setItem('userID', Response.data._id);
+            setLoggedIn(true);
+  
+          }
+          else
+            setMessege({ text: 'invalid details', showMessege: true })
+        })
+  
+    }
+  })
 
+  
 }
 
 
