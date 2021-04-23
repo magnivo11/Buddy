4cbc2d12c59dea311784ef8c516a62e285242102
Nivo <magnivo11@gmail.com>
2021-04-23 21:24:47 +0300

    plant color uses real data from sensor

--- client/src/components/PlantComponent.js
+++ client/src/components/PlantComponent.js
@@ -10,44 +10,29 @@ import axios from 'axios'
  
 export default function PlantComponent({ plantName, plantid, plantsensorid }) { 
     const StatusColor = () => {
-        var status1 = 0;
-        var status2 = 0;
-        var status3 = 0;
  
-        const [sensor, setSensor] = React.useState();
+        const [plant, setPlant] = React.useState();
         React.useEffect(() => {
-            if (plantsensorid != null) {
-                var url = 'http://localhost:8080/sensor/' + plantsensorid;
+            if (plantid != null) {
+                var url = 'http://localhost:8080/plant/' + plantid;
                 axios.get(url).then((Response) => {
-                    if (!sensor) {
-                        setSensor(Response.data);
+                    if (!plant) {
+                        setPlant(Response.data);
                     }
                 })
             }
         }, []);
-
-        if (sensor != null && sensor.light.length > 15) {
-            for (let i = 0; i < 10; i++) {
-                status1 += sensor.light[i].curLight;
-                status2 += sensor.temperature[i].curTemp;
-                status3 += sensor.soilMoisture[i].curMoist;
-            }
-
-            if ((status1 / 10) > 44.5 && ((status1 / 10) < 55) || 
-            (status2 / 10) > 20 && (status2 / 10) < 25 || 
-            (status3 / 10) > 20 && (status2 / 10) < 22) {
-                return statusGreen;
-            }
-            if ((status1 / 10) >= 55 && ((status1 / 10) <= 80) || 
-            (status1 / 10) >= 30 && ((status1 / 10) <= 44.5) || 
-            (status2 / 10) >= 25 && (status2 / 10) <= 40 || 
-            (status2 / 10) >= 10 && (status2 / 10) <= 25 || 
-            (status3 / 10) > 22 && (status2 / 10) < 100) {
-                return statusYellow;
-            }
-             else {return statusRed;}
-        }         
-        else {return statusBlue;}
+        if(plant)
+            if (plant.sensorID != null) {
+                if(plant.healthStatus==3)
+                    return statusRed
+                if(plant.healthStatus==2)
+                    return statusYellow
+                    return statusGreen
+            }         
+                else {
+                    return statusBlue;
+                }
     }
 
 return (
